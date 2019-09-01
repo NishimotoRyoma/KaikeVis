18310a60fb0bfdba7823d93cac58b83b000eabf5
NishimotoRyoma <54461659+NishimotoRyoma@users.noreply.github.com>
2019-09-02 02:05:12 +0900

    Add files via upload

--- server.R
+++ server.R
@@ -7,6 +7,7 @@ library(igraph)
 library(DT)
 library(shinyjs)
 library(colourpicker)
+library(rhandsontable)
 
 
 #-----------#
@@ -16,110 +17,92 @@ library(colourpicker)
 #shinyServer(){...}において、Server内の動きを規定する
 
 shinyServer(function(input,output){
-  
-  
-  ###########見本データの作成############
-  
-  #------------#
-  # sampleData #
-  #------------#
-  #testData : 見本データ
-  #testDataと同じ形のデータを入力する必要がある。
-  #nodeS,nodeF,edgelabel,Date
-  
-  #nodeS : generic type(現在日本語非対応)
-  #         ノードの始点( 支出点 )
-  #nodeF : generic type(現在日本語非対応)
-  #         ノードの終点( 収入点 )
-  #edgelabel : numeric type
-  #         取引の増減額
-  #Date : yyyy-mm-dd
-  #         日付。例えば1994-09-19などの形
-  
-  
-  #------------#
-  # parameters #
-  #------------#
-  #dataNum : int
-  #         見本データの個数
-  #nodeLabel : a~oのアルファベット
-  #           ノードの作成。今回はa~oと名前をつけておいた
-  #nodeS,nodeF,edgeLabel,dateSeq : 先ほど説明したデータ
-  dataNum = 20;
-  nodeLabel = letters[1:15];
-  nodeS = c();
-  nodeF = c();
-  edgeLabel = c();
-  dateSeq = c();
-  
-  #---------#
-  # process #
-  #---------#
-  #データの加工と作成
-  for(i in 1:dataNum)
-  {
-    #nodeS,nodeF,edgeLabelの作成
-    temp = sample(nodeLabel,2);
-    nodeS[i] <- temp[1];
-    nodeF[i] <- temp[2];
-    edgeLabel[i] <- sample(seq(100,900,100),1);
-  }
-  
-  initDate = as.Date("2019-01-01");
-  for(i in 1:(dataNum/5))
-  {
-    #Dateの作成
-    initDate = initDate+1;
-    dateSeq[((i-1)*5+1):((i)*5)] <- rep(initDate,5);
-  }
-  dateSeq = as.character(as.Date(dateSeq,origin="1970-01-01"));
-  
-  #サンプル用データ(testData) : dataframeとして保存
-  testData = data.frame(
+    
+    
+    ###########見本データの作成############
+    
+    #------------#
+    # sampleData #
+    #------------#
+    #testData : 見本データ
+    #testDataと同じ形のデータを入力する必要がある。
+    #nodeS,nodeF,edgelabel,Date
+    
+    #nodeS : generic type(現在日本語非対応)
+    #         ノードの始点( 支出点 )
+    #nodeF : generic type(現在日本語非対応)
+    #         ノードの終点( 収入点 )
+    #edgelabel : numeric type
+    #         取引の増減額
+    #Date : yyyy-mm-dd
+    #         日付。例えば1994-09-19などの形
+    
+    
+    #------------#
+    # parameters #
+    #------------#
+    #dataNum : int
+    #         見本データの個数
+    #nodeLabel : a~oのアルファベット
+    #           ノードの作成。今回はa~oと名前をつけておいた
+    #nodeS,nodeF,edgeLabel,dateSeq : 先ほど説明したデータ
+    dataNum = 20;
+    nodeLabel = letters[1:15];
+    nodeS = c();
+    nodeF = c();
+    edgeLabel = c();
+    dateSeq = c();
+    
+    #---------#
+    # process #
+    #---------#
+    #データの加工と作成
+    for(i in 1:dataNum)
+    {
+        #nodeS,nodeF,edgeLabelの作成
+        temp = sample(nodeLabel,2);
+        nodeS[i] <- temp[1];
+        nodeF[i] <- temp[2];
+        edgeLabel[i] <- sample(seq(100,900,100),1);
+    }
+    
+    initDate = as.Date("2019-01-01");
+    for(i in 1:(dataNum/5))
+    {
+        #Dateの作成
+        initDate = initDate+1;
+        dateSeq[((i-1)*5+1):((i)*5)] <- rep(initDate,5);
+    }
+    dateSeq = as.character(as.Date(dateSeq,origin="1970-01-01"));
+    
+    #サンプル用データ(testData) : dataframeとして保存
+    testData = data.frame(
     nodeS=nodeS,
     nodeF=nodeF,
     edgeLabel=edgeLabel,
     Date=dateSeq
     );
-  
-  #作成した見本データをuiへ渡すためにrenderDataTableを通して変数tableへ格納
-  output$table <- renderDataTable(testData);
-  
-  #uiのタイトル
-  output$tableText=renderText({"以下のような形式のデータセットを用いる"});
-  
-  ###########見本データの作成終わり############
-  
-  ###########データアップロード後の振る舞い############
-  
-  #-----------#
-  # csvUpload #
-  #-----------#
-  #CSVをアップロードしたあとに発生する処理を記述する。
-  #アップロードしたファイルはUIのmydataに保存される。
-  #observeEvent(input$mydata,{...})内の処理はmydataにデータが入って初めて実行される
-  observeEvent(
-    input$mydata,{
-      output$tableText=renderText({"アップロード済みデータ"});
-  
-      #observe({...})内の処理は中のinputが変更されるたびに再実行される
-      observe({
-        #-----------#
-        # inputList #
-        #-----------#
-        #以下のinput変数が変更された場合、再実行される
-        input$eCurve
-        input$eColor
-        input$elcolor
-        input$vColor
-        input$vlcolor
-        input$vfColor
-        input$do1
-        input$vSize
-        input$upper
-        input$lower
-        input$layoutIndex
     
+    #作成した見本データをuiへ渡すためにrenderDataTableを通して変数tableへ格納
+    output$table <- renderDataTable(testData);
+    
+    #uiのタイトル
+    output$tableText=renderText({"以下のような形式のデータセットを用いる"});
+    output$dynamicText <- renderRHandsontable({rhandsontable(data.frame(node="",init=0))})
+    ###########見本データの作成終わり############
+    
+    ###########データアップロード後の振る舞い############
+    
+    #-----------#
+    # csvUpload #
+    #-----------#
+    #CSVをアップロードしたあとに発生する処理を記述する。
+    #アップロードしたファイルはUIのmydataに保存される。
+    #observeEvent(input$mydata,{...})内の処理はmydataにデータが入って初めて実行される
+    observeEvent(
+    input$mydata,{
+        output$tableText=renderText({"アップロード済みデータ"});
+        
         #-------------#
         # outputTable #
         #-------------#
@@ -127,19 +110,19 @@ shinyServer(function(input,output){
         name <- names(input$mydata);
         csv_file <- reactive(read.csv(text=input$mydata[[name]]));
         output$table <- DT::renderDataTable(csv_file());
-
+        
         #--------------------#
         # graphVisualization #
         #--------------------#
         #アップロードしたcsvファイルを次のタブで可視化する
         #csvファイルをDataに入れる
         Data <- csv_file();
-
-  
+        
+        
         #uiから受け取ったbins情報をもとに階級幅の生成
         time_ <- unique(Data$Date);
-
-
+        
+        
         #####プロットの生成
         #####まず最も利用されるノードを求めて、一番前に置く(中心点になる)
         #NodeLabels : アップロードしたデータに利用されているノードのラベル
@@ -149,37 +132,37 @@ shinyServer(function(input,output){
         NodeNum=c();
         for(i in NodeLabels)
         {
-          temp1=c();
-          temp2=c();
-          if(sum(rownames(tempMat)==i)>0)
-          {
-            temp1 <- sum(tempMat[i,]);
-            temp1[is.na(temp1)] <- 0;
-          }
-          if(sum(colnames(tempMat)==i)>0)
-          {    
-            temp2 <- sum(tempMat[,i]);
-            temp2[is.na(temp2)] <- 0;
-          }
-          if(is.null(temp1)) temp1=0;
-          if(is.null(temp2)) temp2=0;
-    
-          NodeNum[which(NodeLabels==i)] = sum(c(temp1,temp2));
+            temp1=c();
+            temp2=c();
+            if(sum(rownames(tempMat)==i)>0)
+            {
+                temp1 <- sum(tempMat[i,]);
+                temp1[is.na(temp1)] <- 0;
+            }
+            if(sum(colnames(tempMat)==i)>0)
+            {
+                temp2 <- sum(tempMat[,i]);
+                temp2[is.na(temp2)] <- 0;
+            }
+            if(is.null(temp1)) temp1=0;
+            if(is.null(temp2)) temp2=0;
+            
+            NodeNum[which(NodeLabels==i)] = sum(c(temp1,temp2));
         }
-  
+        
         #NodeLabelsの順序変換
         tempLabels=c();
         tempLabels[1] <- NodeLabels[which.max(NodeNum)];
         tempLabels[2:length(NodeLabels)] <- NodeLabels[-which.max(NodeNum)];
         NodeLabels=tempLabels;
-  
+        
         #固定ノードの生成
         nL=c();
         for(i in NodeLabels)
         {
-          nL[which(NodeLabels==i)] <- i;
+            nL[which(NodeLabels==i)] <- i;
         }
-  
+        
         g=graph(c("None","None"),isolates=nL);
         g=delete_edges(g,E(g)[1]);
         g=delete_vertices(g,"None");
@@ -193,116 +176,150 @@ shinyServer(function(input,output){
         layoutList[[5]]=layout_as_tree(g);
         layoutList[[6]]=layout_on_sphere(g);
         l=layoutList[[as.numeric(input$layoutIndex)]];
-  
-  
-  
-  
-        #ノードのラベルの決定
-        nodeList=rep(0,length(nL));
-        labelName=paste(V(g)$name,"\n",sep="");
-        labelName=paste(labelName,nodeList,sep="");
-        #labelName=paste(labelName,"yen",sep="");
-        V(g)$label=labelName;
-  
-  
-        graph_=list();
-        graph_[[1]] <- g;
-        name_=c(" ",as.character(as.Date(time_,origin="1970-01-01")));
-        Data_=list();
-        Data_[[1]] <- data.frame(nodeS=c(),nodeF=c(),edgeLabel=c());
+                     
+                     
+                    #ノードのラベルの決定
+                nodeListDataFrame=data.frame(node=nL,init=rep(0,length(nL)));
+                nodeList=nodeListDataFrame$init;
+                 labelName=paste(V(g)$name,"\n",sep="");
+                 labelName=paste(labelName,nodeList,sep="");
+                 #labelName=paste(labelName,"yen",sep="");
+                 V(g)$label=labelName;
+                 
+                 
+                 graph_=list();
+                 graph_[[1]] <- g;
+                 name_=c(" ",as.character(as.Date(time_,origin="1970-01-01")));
+                 Data_=list();
+                 Data_[[1]] <- data.frame(nodeS=c(),nodeF=c(),edgeLabel=c());
         
-        #upper以上の額のエッジは出力しない
-        upper=input$upper
-        if(is.null(upper)==FALSE)
-        {
-          if(is.na(upper)==FALSE)
-          {
-            Data=Data[Data$edgeLabel<upper,]
-          }
-        }
-        #lower以下の額のエッジは出力しない
-        lower=input$lower
-        if(is.null(lower)==FALSE){
-          if(is.na(lower)==FALSE){
-            Data=Data[Data$edgeLabel>lower,]
-          }
-        }
-        #グラフの生成
-        i = 1;
-        for(t in time_)
-        {
-          Data_[[which(time_==t)+1]] <- Data[Data$Date==t,1:3];
-          if(sum(Data$Date==t)>0)
-          {
-            num = 0;
-            for(i in 1:dim(Data)[1])
-            {
-              if(Data$Date[i]==t)
-              {
-                num = num+1;
-                #factorの水準値が出てくるのでcharに変換
-                g = add_edges(g,c(as.character(Data$nodeS[i]),as.character(Data$nodeF[i])));
-                E(g)$label[num] = Data$edgeLabel[i];
+        #動的に値を変更できるrhandsontableでノードリストを引き渡す
+        output$dynamicText <- renderRHandsontable({rhandsontable(nodeListDataFrame)})
         
-                nodeList[which(V(g)$name==as.character(Data$nodeS[i]))] = nodeList[which(V(g)$name==as.character(Data$nodeS[i]))] - Data$edgeLabel[i];
-                nodeList[which(V(g)$name==as.character(Data$nodeF[i]))] = nodeList[which(V(g)$name==as.character(Data$nodeF[i]))] + Data$edgeLabel[i];
-              }
-            }
-          
-            if(input$do1%%2==0)
+        
+        #observe({...})内の処理は中のinputが変更されるたびに再実行される
+        observe({
+            #-----------#
+            # inputList #
+            #-----------#
+            #以下のinput変数が変更された場合、再実行される
+            input$eCurve
+            input$eColor
+            input$elcolor
+            input$vColor
+            input$vlcolor
+            input$vfColor
+            input$do1
+            input$vSize
+            input$upper
+            input$lower
+            input$layoutIndex
+            input$dynamicText
+            
+            #初期リストがuiで変更された場合にそれを反映する
+            nodeListData=hot_to_r(input$dynamicText)
+            nodeList=nodeListData$init;
+            labelName=paste(V(g)$name,"\n",sep="");
+            labelName=paste(labelName,nodeList,sep="");
+            #labelName=paste(labelName,"yen",sep="");
+            V(g)$label=labelName;
+            graph_[[1]] <- g;
+            name_=c(" ",as.character(as.Date(time_,origin="1970-01-01")));
+            Data_[[1]] <- data.frame(nodeS=c(),nodeF=c(),edgeLabel=c());
+
+            
+            #upper以上の額のエッジは出力しない
+            upper=input$upper
+            if(is.null(upper)==FALSE)
             {
-              labelName <- paste(V(g)$name,"\n",sep="");
-              labelName <- paste(labelName,nodeList,sep="");
-              #labelName <- paste(labelName,"yen",sep="");
-              V(g)$label <- labelName;
+                if(is.na(upper)==FALSE)
+                {
+                    Data=Data[Data$edgeLabel<upper,]
+                }
             }
-            else
+            #lower以下の額のエッジは出力しない
+            lower=input$lower
+            if(is.null(lower)==FALSE){
+                if(is.na(lower)==FALSE){
+                    Data=Data[Data$edgeLabel>lower,]
+                }
+            }
+            #グラフの生成
+            i = 1;
+            for(t in time_)
             {
-              V(g)$label <- V(g)$name;
+                Data_[[which(time_==t)+1]] <- Data[Data$Date==t,1:3];
+                if(sum(Data$Date==t)>0)
+                {
+                    num = 0;
+                    for(i in 1:dim(Data)[1])
+                    {
+                        if(Data$Date[i]==t)
+                        {
+                            num = num+1;
+                            #factorの水準値が出てくるのでcharに変換
+                            g = add_edges(g,c(as.character(Data$nodeS[i]),as.character(Data$nodeF[i])));
+                            E(g)$label[num] = Data$edgeLabel[i];
+                            
+                            nodeList[which(V(g)$name==as.character(Data$nodeS[i]))] = nodeList[which(V(g)$name==as.character(Data$nodeS[i]))] - Data$edgeLabel[i];
+                            nodeList[which(V(g)$name==as.character(Data$nodeF[i]))] = nodeList[which(V(g)$name==as.character(Data$nodeF[i]))] + Data$edgeLabel[i];
+                        }
+                    }
+                    
+                    if(input$do1%%2==0)
+                    {
+                        labelName <- paste(V(g)$name,"\n",sep="");
+                        labelName <- paste(labelName,nodeList,sep="");
+                        #labelName <- paste(labelName,"yen",sep="");
+                        V(g)$label <- labelName;
+                    }
+                    else
+                    {
+                        V(g)$label <- V(g)$name;
+                    }
+                }
+                graph_[[which(time_==t)+1]] <- g;
+                g <- delete_edges(g,E(g));
             }
-          }
-          graph_[[which(time_==t)+1]] <- g;
-          g <- delete_edges(g,E(g));
-        }
-
-  
-        #uiでinputしたeCurve,color,sizeを反映させる
-        eCurve=input$eCurve
-        eColor=input$eColor
-        elColor=input$elColor
-  
-        vColor=input$vColor
-        vlColor=input$vlColor
-        vfColor=input$vfColor
-        vSize=input$vSize
-  
-        output$distPlot <- renderPlot({
-          par(family="HiraKakuProN-W3");
-          plot(
-            graph_[[input$bins+1]],
-            layout=l,
-            edge.arrow.size=0.5,
-            edge.curved=eCurve,
-            edge.color=eColor, 
-            edge.label.color=elColor, 
-            vertex.color=adjustcolor(c(vColor),alpha=0.8), 
-            vertex.size=vSize, 
-            vertex.frame.color=vfColor, 
-            vertex.label.color=vlColor, 
-            vertex.label.cex=1.0, 
-            vertex.label.dist=0,
-            main=name_[input$bins+1]
-          )
-
-
+            
+            
+            #uiでinputしたeCurve,color,sizeを反映させる
+            eCurve=input$eCurve
+            eColor=input$eColor
+            elColor=input$elColor
+            
+            vColor=input$vColor
+            vlColor=input$vlColor
+            vfColor=input$vfColor
+            vSize=input$vSize
+            
+            output$distPlot <- renderPlot({
+                par(family="HiraKakuProN-W3");
+                plot(
+                graph_[[input$bins+1]],
+                layout=l,
+                edge.arrow.size=0.5,
+                edge.curved=eCurve,
+                edge.color=eColor,
+                edge.label.color=elColor,
+                vertex.color=adjustcolor(c(vColor),alpha=0.8),
+                vertex.size=vSize,
+                vertex.frame.color=vfColor,
+                vertex.label.color=vlColor,
+                vertex.label.cex=1.0,
+                vertex.label.dist=0,
+                main=name_[input$bins+1]
+                )
+                
+                
+            })
+            
+            output$smallTable <- DT::renderDataTable(
+            Data_[[input$bins+1]],
+            options=list(pageLength=5)
+            )
+            
         })
-  
-        output$smallTable <- DT::renderDataTable(
-          Data_[[input$bins+1]],
-          options=list(pageLength=5)
-        )
-  
-    }) 
-  })
+    })
 })
 ###########アップロード後の振る舞い終わり############
--- ui.R
+++ ui.R
@@ -7,6 +7,7 @@ library(igraph)
 library(DT)
 library(shinyjs)
 library(colourpicker)
+library(rhandsontable)
 
 #---------------#
 # userInterface #
@@ -14,186 +15,200 @@ library(colourpicker)
 #userが利用する画面の規定
 
 shinyUI(
-  navbarPage(
-    "会計可視化",
-    tabPanel(
-      "データの入力",
-      tags$head(
-        tags$link(
-          rel = "stylesheet",
-          href = "styles.css",
-          type = "text/css"
-        ),
-        tags$script(src = "drag.js")
-      ),
-      sidebarLayout(
-        sidebarPanel(
-          h4("赤線にデータをドロップ"),
-          div(
-            id = "drop-area",
-            ondragover = "f1(event)",
-            ondrop = "f2(event)"
-          )
-        ),
-        mainPanel(
-          h4(textOutput("tableText")),
-          DT::dataTableOutput("table")
-        )
-      )
-    ),
-    tabPanel(
-      "可視化",
-      titlePanel("会計可視化"),
-      
-      fluidRow(
-        column(3,
-          dataTableOutput("smallTable")
-        ),
-        
-        column(1),
-             
-        column(8,
-          #グラフの表示部分
-          mainPanel(
-            plotOutput("distPlot")
-          )
-        )
-      ),
-    
-      fluidRow(
-        column(3,
-          numericInput(
-            "eCurve",
-            "矢印の曲率(0以上)",
-            value=0.1,
-            min=0,
-            step=0.1
-          )
-        ),
+navbarPage(
+"会計可視化",
+tabPanel(
+"データの入力",
+tags$head(
+tags$link(
+rel = "stylesheet",
+href = "styles.css",
+type = "text/css"
+),
+tags$script(src = "drag.js")
+),
 
-        column(6,
-          sliderInput(
-            "bins",
-            "",
-            min = 0,
-            max = 20,
-            value = 0
-          ),
-          actionButton("do1","残高表示/非表示")
+fluidRow(
+  column(3,
+h4("赤線にデータをドロップ"),
+div(
+id = "drop-area",
+ondragover = "f1(event)",
+ondrop = "f2(event)"
+)
 
-        ),
-        
-        column(3)
-        
-      )
-    ),
-  
-    tabPanel(
-      "詳細設定",
-      titlePanel("詳細設定"),
-      fluidRow(
-        column(4,
-          colourInput(
-            "vColor",
-            "ノードの色",
-            value="gold",
-            palette="square",
-            allowTransparent=TRUE
-          )
-        ),
-             
-        column(4,
-          colourInput(
-            "eColor",
-            "エッジの色",
-            value="black",
-            palette="square",
-            allowTransparent=TRUE
-          )
-        ),
-        
-        column(4,
-          colourInput(
-            "vfColor",
-            "ノードのフレームの色",
-            value="grey",
-            palette="square",
-            allowTransparent=TRUE
-          )
-        )
-      ),
-      
-      fluidRow(
-        column(4,
-          colourInput(
-            "vlColor",
-            "ノードのラベルの色",
-            value="black",
-            palette="square",
-            allowTransparent=TRUE
-          )
-        ),
-        
-        column(4,
-          colourInput(
-            "elColor",
-            "エッジのラベルの色",
-            value="blue",
-            palette="square",
-            allowTransparent=TRUE
-          )
-        )
-      ),
-      
-      fluidRow(
-        column(4,
-          numericInput(
-            "vSize",
-            "ノードのサイズ(0以上)",
-            value=20,
-            min=0,
-            step=1
-          )
-        ),
-        
-        column(4,
-               numericInput(
-                 "upper",
-                 "金額の上限(入力値以上のエッジを省略)",
-                 value=9999999999,
-                 min=0,
-                 step=1
-               )
-        ),
-        
-        column(4,
-               numericInput(
-                 "lower",
-                 "金額の下限(入力値以下のエッジを省略)",
-                 value=-0.00000001,
-                 min=0,
-                 step=1
-               )
-        )
-      ),
-      
-      fluidRow(
-        column(4,
-               selectInput(
-                 "layoutIndex",
-                 "ノードの配置",
-                 choices=list(
-                   "Star" = 1,
-                   "Circle" = 2,
-                   "Nicely" = 3,
-                   "Random" = 4,
-                   "Tree" = 5,
-                   "Sphere" = 6
-                 ),
-                 selected = 1
-               )
-        )
-      )
-    )
-  )
+),
+column(9,
+h4(textOutput("tableText")),
+DT::dataTableOutput("table")
+)
+)
+),
+tabPanel("初期値の入力",
+fluidRow(column(3),
+         column(6,
+                align="center",
+                h4("ダブルクリックで金額の初期値(init)を変更できる")),
+         column(3)
+         ),
+column(4),
+column(4,
+       rHandsontableOutput("dynamicText")),
+column(4)
+),
+tabPanel(
+"可視化",
+titlePanel("会計可視化"),
+
+fluidRow(
+column(3,
+dataTableOutput("smallTable")
+),
+
+column(1),
+
+column(8,
+#グラフの表示部分
+mainPanel(
+plotOutput("distPlot")
+)
+)
+),
+
+fluidRow(
+column(3,
+numericInput(
+"eCurve",
+"矢印の曲率(0以上)",
+value=0.1,
+min=0,
+step=0.1
+)
+),
+
+column(6,
+sliderInput(
+"bins",
+"",
+min = 0,
+max = 20,
+value = 0
+),
+actionButton("do1","残高表示/非表示")
+
+),
+
+column(3)
+
+)
+),
+
+tabPanel(
+"詳細設定",
+titlePanel("詳細設定"),
+fluidRow(
+column(4,
+colourInput(
+"vColor",
+"ノードの色",
+value="gold",
+palette="square",
+allowTransparent=TRUE
+)
+),
+
+column(4,
+colourInput(
+"eColor",
+"エッジの色",
+value="black",
+palette="square",
+allowTransparent=TRUE
+)
+),
+
+column(4,
+colourInput(
+"vfColor",
+"ノードのフレームの色",
+value="grey",
+palette="square",
+allowTransparent=TRUE
+)
+)
+),
+
+fluidRow(
+column(4,
+colourInput(
+"vlColor",
+"ノードのラベルの色",
+value="black",
+palette="square",
+allowTransparent=TRUE
+)
+),
+
+column(4,
+colourInput(
+"elColor",
+"エッジのラベルの色",
+value="blue",
+palette="square",
+allowTransparent=TRUE
+)
+)
+),
+
+fluidRow(
+column(4,
+numericInput(
+"vSize",
+"ノードのサイズ(0以上)",
+value=20,
+min=0,
+step=1
+)
+),
+
+column(4,
+numericInput(
+"upper",
+"金額の上限(入力値以上のエッジを省略)",
+value=9999999999,
+min=0,
+step=1
+)
+),
+
+column(4,
+numericInput(
+"lower",
+"金額の下限(入力値以下のエッジを省略)",
+value=-0.00000001,
+min=0,
+step=1
+)
+)
+),
+
+fluidRow(
+column(4,
+selectInput(
+"layoutIndex",
+"ノードの配置",
+choices=list(
+"Star" = 1,
+"Circle" = 2,
+"Nicely" = 3,
+"Random" = 4,
+"Tree" = 5,
+"Sphere" = 6
+),
+selected = 1
+)
+)
+)
+)
+)
 )
